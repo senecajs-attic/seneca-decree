c5248014262d927df60ae90036bf3d76fa87c4b1
Dean McDonnell <mcdonnelldean@outlook.com>
2016-03-01 13:51:40 +0000

    Add comments

--- decree.js
+++ decree.js
@@ -6,6 +6,8 @@ const Patrun = require('Patrun')()
 const _ = require('lodash')
 
 module.exports = (opts, scripts, ready) => {
+
+  // A catch all in case we get no match.
   let catch_all = (opts, server, done) => {
     const seneca = server.seneca
     seneca.ready(() => {
@@ -14,21 +16,28 @@ module.exports = (opts, scripts, ready) => {
     })
   }
 
+  // map each script to it's pattern and
+  // update the catch_all if one is provided
   _.each(scripts, (map) => {
     if (map.pin) Patrun.add(map.pin, map.script)
     else catch_all = map.script
   })
 
+  // get our input and based on it, our pattern
   const input = Minimist(process.argv.slice(2))._[0] || ''
   const pattern = Jsonic(input)
 
+  // match our pattern or use the catch_all.
   let match = Patrun.find(pattern)
   if (match) match = wrap(match)
   else match = wrap(catch_all)
 
+  // run the hapi server with the
+  // selected scrpt passed in.
   return ready(opts, match)
 }
 
+// handle any errors
 function endOnError (err) {
   if (err) {
     console.log(err)
@@ -36,6 +45,8 @@ function endOnError (err) {
   }
 }
 
+// some wrapping to keep script pluggability
+// simple to reckon about. Maps script call.
 function wrap (inner) {
   const wrapper = (err, opts, server) => {
     endOnError(err)
